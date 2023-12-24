827a43f5d15be3984b353f66357d32a245e08cce
Faelinrk <faelinrk@mail.ru>
2023-12-24 16:30:21 +0300

    Created method CountElementNum

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 ﻿class Program{
     static void Main(string[] args){
-        
+        string[] firstArray = {"Арбузы", "Бананы", "Мак", "Акакий", "Три"};
+    }
+    static int CountNumEnements(string[] array, int elementLen){
+        int count = 0;
+        for (int i = 0; i < array.Length; i++){
+            if (array[i].Length <= elementLen){
+                count ++;
+            }
+        }
+        return count;
     }
 }
